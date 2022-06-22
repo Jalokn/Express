a423e899d9c913326d55f420d391504b7d8ccf95
Jacob Knudson <jacoblknudson@gmail.com>
2022-06-22 15:58:54 -0600

    finshed Setting up all CRUD on the product list and created a filter by category option

--- mongoose_Express/index.js
+++ mongoose_Express/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const path = require('path')
 const mongoose = require('mongoose');
+const methodOveride = require('method-override')
+
 
 const Product = require('./models/product')
 mongoose.connect('mongodb://localhost:27017/farmStand')
@@ -15,9 +17,13 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }))
+app.use(methodOveride('_method'))
+
+
+const categories = ['fruit', 'vegetable', 'dairy',]
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new')
+    res.render('products/new', { categories })
 })
 
 app.post('/products', async (req, res) => {
@@ -28,17 +34,44 @@ app.post('/products', async (req, res) => {
 })
 
 app.get('/products', async (req, res) => {
-    const products = await Product.find({})
-    res.render('products/index', { products })
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category: category })
+        res.render('products/index', { products, category })
+
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+
 })
+
 
 app.get('/products/:id', async (req, res) => {
     const { id } = req.params
     const product = await Product.findById(id)
-    console.log(product)
     res.render('products/show', { product })
 })
 
+app.get('/products/:id/edit', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findById(id)
+    res.render('products/edit', { product, categories })
+})
+
+app.put('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true })
+    await product.save()
+    console.log(req.body)
+    res.redirect(`/products/${product._id}`)
+})
+
+app.delete('/products/:id', async (req, res) => {
+    const { id } = req.params
+    const deletedProduct = await Product.findByIdAndDelete(id)
+    res.redirect('/products')
+})
 
 app.listen(3000, () =>
     console.log('Listening on port 3000'))
